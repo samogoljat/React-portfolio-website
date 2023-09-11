37e73872ece2e3c5e62e20a505f531ce86226fa1
samogoljat <samo.goljat1@gmail.com>
2023-09-11 05:37:07 +0200

    changed project files on website

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,39 +1,39 @@
-import Proj1 from "../assets/proj1.jpg";
-import Proj2 from "../assets/proj2.jpg";
-import Proj3 from "../assets/proj3.webp";
-import Proj4 from "../assets/proj4.webp";
-import Proj5 from "../assets/proj5.jpg";
-import Proj6 from "../assets/proj6.png";
+import Proj1 from "../assets/avtobus/avtobus.png";
+import Proj2 from "../assets/ProjectNightOwl/projectnightowl.png";
+import Proj3 from "../assets/TeaKejk/Teakejk.png";
+import Proj4 from "../assets/mining/mining.png";
+import Proj5 from "../assets/izpis_streznikov/izpis.png";
+import Proj6 from "../assets/ubl/ubl.png";
 
 export const ProjectList = [
   {
-    name: "Dijkstra Algorithm Visualizer",
+    name: "Avtobus d.o.o Website",
     image: Proj1,
-    skills: "JavaScript,HTML,CSS",
+    skills: "HTML,CSS",
   },
   {
-    name: "Ecommerce Website",
+    name: "Wordpress Website about Yt channel",
     image: Proj2,
-    skills: "React,Node.js,MongoDB",
+    skills: "Wordpress, PHP",
   },
   {
-    name: "Spotify Clone",
+    name: "Wife backing Website",
     image: Proj3,
-    skills: "React,Node.js,MongoDB,SpotifyAPI",
+    skills: "HTML, CSS",
   },
   {
-    name: "Social Media Website",
+    name: "Website with instruction to mine Ethereum",
     image: Proj4,
-    skills: "React,Node.js,MySQL,GraphQL",
+    skills: "HTML, CSS, Javascript",
   },
   {
-    name: "Dashboard Visualizer",
+    name: "Website for dynamic output JSON data from Icinga2 api",
     image: Proj5,
-    skills: "JavaScript,HTML,CSS",
+    skills: "HTML,CSS, Javascript, JSON, bash",
   },
   {
-    name: "Mobile Game",
+    name: "Truck company website",
     image: Proj6,
-    skills: "React Native,JavaScript,HTML,CSS",
+    skills: "HTML, CSS",
   },
 ];
